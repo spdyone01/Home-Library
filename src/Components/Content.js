063ac6ec84088c1f13a194f5357e91a6a9cd2a0c
DarkNesce <DarkNesce@users.noreply.github.com>
2021-08-10 17:53:44 -0600

    start Content Component but not implement

---
+++ src/Components/Content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Content extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        // here we need to render 'My Library' by default and re-render when user
+        // clicks something else or filters items
+
+        return(
+            <div className='content'>
+
+            </div>
+        )
+    }
+}
