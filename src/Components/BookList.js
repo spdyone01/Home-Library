9e1ac12ddd615e6058a70defae5aa0cb69652513
DarkNesce <DarkNesce@users.noreply.github.com>
2021-08-11 00:19:01 -0600

    edit ternary logic to return proper data

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -21,7 +21,7 @@ class BookList extends React.Component {
                     author = { book.author_name[0] ? book.author_name[0] : '' }
                     publishedYear = { book.first_publish_year ? book.first_publish_year : 0 }
                     publishedBy = { book.publisher[0] ? book.publisher[0] : '' }
-                    genre = { (typeof book.subject === "undefined") || book.subject[0] ? '' : book.subject[0] }
+                    genre = { (typeof book.subject === "undefined") || !book.subject[0] ? '' : book.subject[0] }
                     ISBN = { book.isbn[0] ? book.isbn[0] : '0000000000000'}
                     cover = { imageAddress ? imageAddress : '/src/images/Where_the_red_fern_grows.jpg' }
                     key = { book.isbn[0] }
