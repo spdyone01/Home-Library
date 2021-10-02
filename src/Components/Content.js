11119cd3f6b1fbd55c7e0577a8ff6139a297802f
spdyone01 <44117380+spdyone01@users.noreply.github.com>
2021-10-02 10:02:11 -0600

    initial commit

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,124 +1,138 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /* Import api libraries */
-import OpenLibrary from '../api/open_library';
+import OpenLibrary from "../api/open_library";
 
 /* Import Components */
-import SearchBar from './SearchBar';
-import BookList from './BookList';
-import Collections from './Collections';
-import ReadingQueue from './ReadingQueue';
-import Favorites from './Favorites';
-import Wanted from './Wanted';
-import ReadHistory from './ReadHistory';
-import AddBook from './AddBook';
-import page404 from './page404';
+import SearchBar from "./SearchBar";
+import BookList from "./BookList";
+import Collections from "./Collections";
+import ReadingQueue from "./ReadingQueue";
+import Favorites from "./Favorites";
+import Wanted from "./Wanted";
+import ReadHistory from "./ReadHistory";
+import AddBook from "./AddBook";
+import page404 from "./page404";
 
 class Content extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchResults: '',
-            searchItems: [],
-            bookList: [],
-            filterText: '',
-            searchType: ['title', 'Author', 'Genre', 'Date Added']
-        }
-    }
-    
-    onSearchSubmit = async (term, searchType) => {
-        const response = await OpenLibrary.get(`/search.json?${searchType}=${term}&limit=5`, {
-            params: ''
-        })
-        // this.setState({ searchResults: response });
-        this.setState({ searchItems: response.data.docs })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: "",
+      searchItems: [],
+      bookList: [],
+      filterText: "",
+      searchType: ["title", "author", "genre", "Date Added"],
+      activeSearchType: 0
+    };
+  }
 
-    filterBooks = (term) => {
-        this.setState({ filterText: term})
-    }
+  onSearchSubmit = async (term, searchType, activeSearchType) => {
+    console.log(this.state.searchType);
+    const response = await OpenLibrary.get(
+      `/search.json?${
+        this.state.searchType[this.state.activeSearchType]
+      }=${term}&limit=5`,
+      {
+        params: ""
+      }
+    );
+    // this.setState({ searchResults: response });
+    this.setState({ searchItems: response.data.docs });
+  };
 
-    render() {
-        
-        return(
-            <div className='content'>
-                    <Switch>    
-                        <Route
-                            path='/addbook' exact
-                            render={() => (
-                                <SearchBar 
-                                    label= { 'Book Search' }
-                                    searchType = { this.state.searchType[0] }
-                                    onSearchSubmit={ this.onSearchSubmit }
-                                />
-                            )}
-                        />
-                        <Route
-                            path='/'
-                            render={() => (
-                                <SearchBar 
-                                    label= { 'Book Filter' }
-                                    onFilterChange={ this.filterBooks }
-                                    searchType={ this.state.searchType[0] }  
-                                />
-                            )}
-                        />
-                    </Switch>
+  filterBooks = (term) => {
+    this.setState({ filterText: term });
+  };
 
-                    <Switch>
-                        <Route 
-                            path='/' exact
-                            render={() => (
-                                <BookList bookList={this.props.bookList} isAuthed={true}/>
-                            )}
-                        />
-                        <Route 
-                            path='/collections' exact
-                            render={() => (
-                                <Collections bookList={this.props.bookList} isAuthed={true}/>
-                            )}
-                        />
-                        <Route 
-                            path='/readingqueue' exact
-                            render={() => (
-                                <ReadingQueue bookList={this.props.bookList} isAuthed={true}/>
-                            )}
-                        />
-                        <Route 
-                            path='/favorites' exact
-                            render={() => (
-                                <Favorites bookList={this.props.bookList} isAuthed={true}/>
-                            )}
-                        />
-                        <Route 
-                            path='/wanted' exact
-                            render={() => (
-                                <Wanted bookList={this.props.bookList} isAuthed={true}/>
-                            )}
-                        />
-                        <Route 
-                            path='/readhistory' exact
-                            render={() => (
-                                <ReadHistory bookList={this.props.bookList} isAuthed={true}/>
-                            )}
-                        />
-                        <Route 
-                            path='/addbook' exact
-                            render={() => (
-                                <AddBook
-                                    bookList={ this.props.bookList } 
-                                    isAuthed={ true }
-                                    addBook={ this.props.addBookToLibrary }
-                                    searchItems= { this.state.searchItems }
-                                />
-                            )}
-                        />
-                        <Route component={page404}/>
-                    </Switch>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="content">
+        <Switch>
+          <Route
+            path="/addbook"
+            exact
+            render={() => (
+              <SearchBar
+                label={"Book Search"}
+                searchType={this.state.searchType}
+                onSearchSubmit={this.onSearchSubmit}
+              />
+            )}
+          />
+          <Route
+            path="/"
+            render={() => (
+              <SearchBar
+                label={"Book Filter"}
+                onFilterChange={this.filterBooks}
+                searchType={this.state.searchType}
+              />
+            )}
+          />
+        </Switch>
+
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <BookList bookList={this.props.bookList} isAuthed={true} />
+            )}
+          />
+          <Route
+            path="/collections"
+            exact
+            render={() => (
+              <Collections bookList={this.props.bookList} isAuthed={true} />
+            )}
+          />
+          <Route
+            path="/readingqueue"
+            exact
+            render={() => (
+              <ReadingQueue bookList={this.props.bookList} isAuthed={true} />
+            )}
+          />
+          <Route
+            path="/favorites"
+            exact
+            render={() => (
+              <Favorites bookList={this.props.bookList} isAuthed={true} />
+            )}
+          />
+          <Route
+            path="/wanted"
+            exact
+            render={() => (
+              <Wanted bookList={this.props.bookList} isAuthed={true} />
+            )}
+          />
+          <Route
+            path="/readhistory"
+            exact
+            render={() => (
+              <ReadHistory bookList={this.props.bookList} isAuthed={true} />
+            )}
+          />
+          <Route
+            path="/addbook"
+            exact
+            render={() => (
+              <AddBook
+                bookList={this.props.bookList}
+                isAuthed={true}
+                addBook={this.props.addBookToLibrary}
+                searchItems={this.state.searchItems}
+              />
+            )}
+          />
+          <Route component={page404} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Content;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,59 +1,76 @@
-import React from 'react';
-import '../styles/SearchBar.css';
+import React from "react";
+import "../styles/SearchBar.css";
 
-class SearchBar extends React.Component { //need a class based component here because we'll use state to handle user input.
-    
-    state = { term: '' };
-    
+class SearchBar extends React.Component {
+  //need a class based component here because we'll use state to handle user input.
 
-    onSearchSubmit = (event) => {
-        event.preventDefault();
-        this.props.onSearchSubmit(this.state.term, this.props.searchType)
+  state = { term: "" };
+
+  onSearchSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSearchSubmit(
+      this.state.term,
+      this.props.searchType,
+      this.props.activeSearchType
+    );
+  };
+
+  onFilterChange = (event) => {
+    this.setState({ term: event.target.value });
+    this.props.onFilterChange(this.state.term);
+    console.log(this.state.term);
+  };
+
+  sortOptions = (options) => {
+    let optionsArray = options.map((option) => (
+      <option value={option}>{option}</option>
+    ));
+    return optionsArray;
+  };
+
+  render() {
+    if (this.props.label === "Book Search") {
+      return (
+        <div className="ui segment search-bar">
+          <form onSubmit={this.onSearchSubmit} className="ui form">
+            <div className="field">
+              <label>{this.props.label}</label>
+              <input
+                type="text"
+                value={this.state.term}
+                onChange={(e) => this.setState({ term: e.target.value })}
+                placeholder="Search for a book here"
+              />
+              <label>Sort By:</label>
+              <select
+                id="sortBy"
+                value={this.props.activeSearchType}
+                onChange={this.props.handleOnChange}
+              >
+                {this.sortOptions(this.props.searchType)}
+              </select>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div className="ui segment search-bar">
+          <form className="ui form" onSubmit={(e) => e.preventDefault()}>
+            <div className="field">
+              <label>{this.props.label}</label>
+              <input
+                type="text"
+                value={this.state.term}
+                onChange={(e) => this.onFilterChange(e)}
+                placeholder={`Filter Books by ${this.props.searchType}`}
+              />
+            </div>
+          </form>
+        </div>
+      );
     }
-
-
-    onFilterChange = (event) => {
-        this.setState({ term: event.target.value })
-        this.props.onFilterChange(this.state.term)
-        console.log(this.state.term)
-    }
- 
-
-    render() { 
-        if(this.props.label === 'Book Search'){
-            return ( 
-                <div className="ui segment search-bar">
-                    <form onSubmit={this.onSearchSubmit} className="ui form">
-                        <div className="field">
-                            <label>{ this.props.label }</label>
-                            <input 
-                                type="text" 
-                                value={this.state.term} 
-                                onChange={(e) => this.setState({ term: e.target.value })}
-                                placeholder='Search for a book here'
-                            />
-                        </div>
-                    </form>
-                </div>  
-            );
-        } else {
-            return (
-                <div className="ui segment search-bar">
-                    <form className="ui form" onSubmit={(e) => e.preventDefault()}>
-                        <div className="field">
-                            <label>{ this.props.label }</label>
-                            <input 
-                                type="text" 
-                                value={this.state.term} 
-                                onChange={(e) => this.onFilterChange(e)}
-                                placeholder={`Filter Books by ${this.props.searchType}`}
-                            />
-                        </div>
-                    </form>
-                </div>
-            )
-        }        
-    };
-};
+  }
+}
 
 export default SearchBar;
